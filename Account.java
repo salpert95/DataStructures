a9768f54038556ed8530240623e4da06e2120529
salpert95 <salpert95@gmail.com>
2015-02-04 05:01:49 -0600

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,22 @@
+public abstract class Account {
+	public Account(){
+		balance = 0;
+	}
+	protected String accountNum;
+	protected double balance;
+	public double calculateDailyInterest() {
+		double interest = 0;
+		if (balance > 500){
+			double premath = .03 / 365;
+			interest = (balance - 500) * premath;
+			balance += interest;
+		}
+		return interest;
+	}
+	void deposit(double amount){
+		balance += amount;
+	}
+	void withdraw(double amount){
+		balance -= amount;
+	}
+}
