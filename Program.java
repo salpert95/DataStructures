b9cb12096b6a27cc839bca7c3627bd9e85e8bce5
salpert95 <salpert95@gmail.com>
2015-02-04 05:02:32 -0600

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,3 +1,6 @@
+//Program by Steven Alpert
+//Assignment 1 - File to string
+//For CIS 269 at Joliet Junior College
 public class Program {
 	public static void main(String[] args) {
 		TestProgram.Instance.Run();
