c923a57bad7b9da7fc1dbca5b0afc7f37a26e381
salpert95 <salpert95@gmail.com>
2015-02-12 11:47:06 -0600

    Create EntryPoint.java
    Assignment 1 created.

---
+++ Assignment1/EntryPoint.java
@@ -0,0 +1,9 @@
+//Program by Steven Alpert
+//Assignment 1 - File to string
+//For CIS 269 at Joliet Junior College
+package com.school.bank;
+public class EntryPoint {
+	public static void main(String[] args){	
+		Bank.instance.Run("G:\\College\\Spring 2015\\data structures\\bank.dat");
+	}
+}
